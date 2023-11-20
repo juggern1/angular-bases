3856cc523e1600ba33e92416ed0509fafb7b2866
Jorge <juggern1@gmail.com>
2023-11-20 11:49:06 +0100

    Componentes nuevos para dbz

---
+++ src/app/dbz/components/AddCharacter/addCharacter.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-character',
+  templateUrl: './addCharacter.component.html',
+  styleUrls: ['./addCharacter.component.css']
+})
+export class AddCharacterComponent { }
